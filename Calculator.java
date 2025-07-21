e8421cf6818610ea85ada0ce061f6477bc1da2ce
Soham Shinde <sohamshindess21@gmail.com>
2025-07-21 19:53:00 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,37 @@
+package Practice;
+
+import java.util.*;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO CALCULATOR");
+        System.out.println("Enter the operation to be done: ");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
+        System.out.println("Enter two nos. "); 
+            int a = sc.nextInt();   
+            int b = sc.nextInt();
+        switch (button) {
+            case 1:
+               System.out.println(a+b);
+                break;
+            case 2:
+               System.out.println(a-b);
+                break;
+            case 3:
+               System.out.println(a*b);
+                break;
+            case 4:
+               System.out.println(a/b);
+                break;
+
+                default:
+                break;
+        }
+        sc.close();
+    }
+}
